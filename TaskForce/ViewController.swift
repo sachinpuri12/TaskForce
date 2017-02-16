226e5451daed98a4640a314bd97d8bf6762c7c25
Sachin Puri <sachinpuri@wustl.edu>
2017-02-15 18:06:02 -0600

    testing commit

--- TaskForce/ViewController.swift
+++ TaskForce/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let i = 64
+        print("i")
     }
 
     override func didReceiveMemoryWarning() {
