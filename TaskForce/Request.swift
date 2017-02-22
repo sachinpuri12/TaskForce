069912f4080259550df4b7983bf2c7047e319d58
Sachin Puri <sachinpuri@wustl.edu>
2017-02-22 11:22:11 -0600

    storyboarding

--- TaskForce/Request.swift
+++ TaskForce/Request.swift
@@ -1,26 +1,28 @@
 //
-//  ViewController.swift
+//  TaskRequest.swift
 //  TaskForce
 //
-//  Created by Sachin Puri on 2/15/17.
+//  Created by Sachin Puri on 2/22/17.
 //  Copyright © 2017 Sachin Puri. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class TaskRequest: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
 
