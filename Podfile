29738757dbd89c252a713f5bcd2862d7c260a6c9
Sachin Puri <sachinpuri@wustl.edu>
2017-03-29 16:50:55 -0500

    added database insert

--- Podfile
+++ Podfile
@@ -5,11 +5,9 @@ target 'TaskForce' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for TaskForce
 pod 'Firebase/Core'
 pod 'Firebase/Messaging'
 pod 'Firebase/Database'
-pod 'Firebase/Invites'
 pod 'Firebase/Auth'
 
   target 'TaskForceTests' do
--- TaskForce/TaskFeed.swift
+++ TaskForce/TaskFeed.swift
@@ -13,19 +13,21 @@ import Firebase
 
 class TaskFeed: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var feedTable: UITableView!
     let nameArray = ["Rexi", "David", "Sachin", "Jimmy"]
     let taskArray = ["Get eggs", "Paint fence", "Wash dishes", "Pick up dry cleaning"]
     let locArray = ["Schnucks", "Beta", "Beta", "Dry Cleaners"]
     let moneyArray = [1, 10, 4, 2]
+    var db = FIRDatabase.database().reference()
+    var tasksArray = [Task]()
     
-    //let dataRef = FIRDatabase.database().reference(withPath: "tasks")
     
-    @IBOutlet weak var feedTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         feedTable.delegate = self
         feedTable.dataSource = self
+        db = FIRDatabase.database().reference()
     }
     
     override func didReceiveMemoryWarning() {
@@ -34,8 +36,28 @@ class TaskFeed: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     override func viewDidAppear(_ animated: Bool) {
         feedTable.reloadData()
+        super.viewDidLoad()
+        db = FIRDatabase.database().reference()
     }
-    
+//    func loadData(){
+//        let snap = db.observeSingleEventOfType(.value, withBlock: { snapshot in
+//            
+//            if !snapshot.exists() { return }
+//            
+//            //print(snapshot)
+//            
+//            if let userName = snapshot.value["full_name"] as? String {
+//                print(userName)
+//            }
+//            if let email = snapshot.value["email"] as? String {
+//                print(email)
+//            }
+//            
+//            // can also use
+//            // snapshot.childSnapshotForPath("full_name").value as! String
+//        })
+//    }
+
     //loading the table
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
