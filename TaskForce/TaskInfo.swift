8dbcb879e0e16cc130d19833fa3a1e84a8e4da48
carneyjimmy <ljames23>
2017-04-05 18:48:31 -0500

    task feed

--- TaskForce/TaskInfo.swift
+++ TaskForce/TaskInfo.swift
@@ -8,12 +8,38 @@
 
 import Foundation
 import UIKit
+import Firebase
 
 class TaskInfo: UIViewController {
+    
+    @IBOutlet weak var Requester: UILabel!
+    @IBOutlet weak var Description: UILabel!
+    @IBOutlet weak var paymentText: UILabel!
+
+    var requestRating: String = ""
+    var location: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        let ref = FIRDatabase.database().reference()
+        ref.child("tasks/\(selectedTask)").observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            let value = snapshot.value as? NSDictionary
+            
+            let name = value?["name"] as? String ?? ""
+            
+            let title = value?["title"] as? String ?? ""
+            let task = value?["description"] as? String ?? ""
+            let place = value?["location"] as? String ?? ""
+            
+            self.Requester.text = name
+            self.Description.text = task
+            
+            
+            
+        })
         
     }
     
